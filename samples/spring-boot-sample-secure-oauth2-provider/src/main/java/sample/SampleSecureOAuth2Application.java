31d40466f83f9b07b62f00fd956eea8d9b2748d9
Josh Cummings <josh.cummings@gmail.com>
2019-02-14 16:53:26 -0700

    Adds /user Endpoint to Authorization Server Sample
    Issue: gh-84

--- samples/spring-boot-sample-secure-oauth2-provider/src/main/java/sample/SampleSecureOAuth2Application.java
+++ samples/spring-boot-sample-secure-oauth2-provider/src/main/java/sample/SampleSecureOAuth2Application.java
@@ -15,9 +15,14 @@
  */
 package sample;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * After you launch the app, you can retrieve a bearer token like this:
@@ -35,7 +40,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @EnableAuthorizationServer
 @SpringBootApplication
+@RestController
+@EnableResourceServer
 public class SampleSecureOAuth2Application {
+
+	@GetMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSecureOAuth2Application.class, args);
